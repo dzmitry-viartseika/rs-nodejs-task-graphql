1e90030e0048a1e74b5dda96fe1acb6cad212614
Dzmitry Viartseika <verteyko1990@gmail.com>
2023-07-22 13:15:23 +0200

    implement task according to requirements

--- src/plugins/env.ts
+++ src/plugins/env.ts
@@ -3,18 +3,18 @@ import { fastifyEnv } from '@fastify/env';
 import { Type, Static } from '@fastify/type-provider-typebox';
 
 const schema = Type.Object({
-  FASTIFY_PORT: Type.Integer(),
+    FASTIFY_PORT: Type.Integer(),
 });
 
 export default fp(async (fastify) =>
-  fastify.register(fastifyEnv, {
-    dotenv: true,
-    schema,
-  }),
+    fastify.register(fastifyEnv, {
+        dotenv: true,
+        schema,
+    }),
 );
 
 declare module 'fastify' {
-  interface FastifyInstance {
-    config: Static<typeof schema>;
-  }
+    interface FastifyInstance {
+        config: Static<typeof schema>;
+    }
 }
