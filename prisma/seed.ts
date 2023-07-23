7a8b6442887d760c476cefaae937b68246344f6f
Dzmitry Viartseika <verteyko1990@gmail.com>
2023-07-23 11:49:35 +0200

    implement task according to requirements

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,14 +3,14 @@ import { MemberType, PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 const memberTypes: MemberType[] = [
-  { id: 'basic', postsLimitPerMonth: 10, discount: 2.3 },
-  { id: 'business', postsLimitPerMonth: 100, discount: 7.7 },
+    { id: 'basic', postsLimitPerMonth: 10, discount: 2.3 },
+    { id: 'business', postsLimitPerMonth: 100, discount: 7.7 },
 ];
 
 for (const memberType of memberTypes) {
-  await prisma.memberType.create({
-    data: memberType,
-  });
+    await prisma.memberType.create({
+        data: memberType,
+    });
 }
 
 await prisma.$disconnect();
